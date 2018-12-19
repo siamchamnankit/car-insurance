0ae4ea095ba7c1e7ff4ff61d3431d26eca42be4e
mondcub <mondcub@gmail.com>
2018-12-19 16:14:23 +0700

    bean user

---
+++ TIM/src/bw0-parent/bw0-web/src/main/java/th/co/toyota/application/web/form/AuthenForm.java
@@ -0,0 +1,19 @@
+package th.co.toyota.application.web.form;
+
+public class AuthenForm {
+	String username;
+	String password;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
