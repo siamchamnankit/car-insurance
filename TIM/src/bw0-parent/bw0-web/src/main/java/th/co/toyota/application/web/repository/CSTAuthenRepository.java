abcabf45e0246604755b74660d9eb4ab40d854f1
thanapontha <thanapontha@gmail.com>
2018-12-19 14:41:09 +0700

    Modify repository

--- TIM/src/bw0-parent/bw0-web/src/main/java/th/co/toyota/application/web/repository/CSTAuthenRepository.java
+++ TIM/src/bw0-parent/bw0-web/src/main/java/th/co/toyota/application/web/repository/CSTAuthenRepository.java
@@ -9,11 +9,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.validation.constraints.NotNull;
 
-import org.hibernate.internal.SessionImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import th.co.toyota.bw0.api.repository.common.IBW00000Repository;
 import th.co.toyota.bw0.api.service.common.UserInfo;
 
 @Repository
@@ -24,22 +25,19 @@ public class CSTAuthenRepository implements ISTAuthenRepository {
 
 	final Logger logger = LoggerFactory.getLogger(CSTAuthenRepository.class);
 
-	
+	@Autowired
+	protected IBW00000Repository commonRepo;
 	
 	@Override
-	public UserInfo authen(Connection conn, String userName, String password){
+	public UserInfo authen(String userName, String password){
 		boolean closeConnection = true;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
-
+		Connection conn = null;
 		UserInfo user = new UserInfo();
         try{
-			if(conn==null){
-				SessionImpl session = (SessionImpl)(em.getDelegate());
-				conn = session.getJdbcConnectionAccess().obtainConnection();
-			}else{
-				closeConnection = false;
-			}
+				conn = commonRepo.getConnection();
+
 			
         	StringBuilder sql = new StringBuilder();
 			sql.append(" SELECT name_lastname ");
--- TIM/src/bw0-parent/bw0-web/src/main/java/th/co/toyota/application/web/repository/ISTAuthenRepository.java
+++ TIM/src/bw0-parent/bw0-web/src/main/java/th/co/toyota/application/web/repository/ISTAuthenRepository.java
@@ -1,12 +1,10 @@
 package th.co.toyota.application.web.repository;
 
-import java.sql.Connection;
-
 import th.co.toyota.bw0.api.service.common.UserInfo;
 
 
 public interface ISTAuthenRepository {
 
-	public UserInfo authen(Connection conn, String userName, String password);
+	public UserInfo authen(String userName, String password);
 	
 }
