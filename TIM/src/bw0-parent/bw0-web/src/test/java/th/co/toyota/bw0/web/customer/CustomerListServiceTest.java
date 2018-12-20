eafda2564925a2dea700af60a7e554a92f601a4f
programmua2204 <programmua.a@gmail.com>
2018-12-20 11:18:58 +0700

    edit sql add file

---
+++ TIM/src/bw0-parent/bw0-web/src/test/java/th/co/toyota/bw0/web/customer/CustomerListServiceTest.java
@@ -0,0 +1,14 @@
+package th.co.toyota.bw0.web.customer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CustomerListServiceTest {
+
+	@Test
+	public void searchCustomerExpireDataShouldBeEmtey() {
+		//searchCustomerExpireData
+	}
+
+}
