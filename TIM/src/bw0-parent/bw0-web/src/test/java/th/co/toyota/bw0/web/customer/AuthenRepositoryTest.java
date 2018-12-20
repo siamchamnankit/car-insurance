0b0ec87335e3772e5051bba0bbf1bd9d76e0b195
yong <yongsukanya@gmail.com>
2018-12-20 16:03:47 +0700

    Add test script

---
+++ TIM/src/bw0-parent/bw0-web/src/test/java/th/co/toyota/bw0/web/customer/AuthenRepositoryTest.java
@@ -0,0 +1,99 @@
+package th.co.toyota.bw0.web.customer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import th.co.toyota.application.config.AppConfig;
+import th.co.toyota.application.web.repository.ISTAuthenRepository;
+import th.co.toyota.bw0.api.repository.common.IBW00000Repository;
+import th.co.toyota.bw0.api.service.common.UserInfo;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class AuthenRepositoryTest {
+	
+	@Autowired
+	private ISTAuthenRepository repository;
+	
+	@Autowired
+	private IBW00000Repository commonRepository;
+	
+
+	String username = "prathan";
+	String password = "3Nj0Y8319";
+	Connection conn = null;
+	
+	@Before
+	public void setUp() {
+//		conn = commonRepository.getConnection();
+//		assertEquals("Open Connection","Open Connection");
+	}
+	
+	@Test
+	public void getUserAuthenWithCorrectUsernameAndPasswordDataShouldBeHaveData() {
+		UserInfo userInfo = repository.authen(username, password);
+		
+		assertEquals("ประธาน ด่านสกุลเจริญกิจ", userInfo.getName());
+		assertEquals("prathan", userInfo.getUserName());
+		assertEquals("3Nj0Y8319", userInfo.getUserPass());
+	}
+	
+	@Test
+	public void getUserAuthenWithIncorrectUsernameDataShouldBeNull() {
+		username = "prathanxxx";
+		
+		UserInfo userInfo = repository.authen(username, password);
+
+		assertTrue(userInfo.getName().isEmpty());
+		assertTrue(userInfo.getUserName().isEmpty());
+		assertTrue(userInfo.getUserPass().isEmpty());
+	}
+
+	@Test
+	public void getUserAuthenWithIncorrectPasswordDataShouldBeNull() {
+		password = "incorrectPassword";
+		
+		UserInfo userInfo = repository.authen(username, password);
+		
+		assertTrue(userInfo.getName().isEmpty());
+		assertTrue(userInfo.getUserName().isEmpty());
+		assertTrue(userInfo.getUserPass().isEmpty());
+	}
+	
+	@Test
+	public void getUserAuthenWithIncorrectUsernameAndPasswordDataShouldBeNull() {
+		username = "prathanxxxxx";
+		password = "incorrectPassword";
+		
+		UserInfo userInfo = repository.authen(username, password);
+		
+		assertTrue(userInfo.getName().isEmpty());
+		assertTrue(userInfo.getUserName().isEmpty());
+		assertTrue(userInfo.getUserPass().isEmpty());
+	}
+
+	
+	@After
+	public void close() {
+		try {
+			if(conn!=null && !conn.isClosed()){
+//				conn.close();
+//				assertEquals("Closed Connection","Closed Connection");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
