e0f4c00b3bf4346fbd385371c1b354ba5c39f15e
programmua2204 <programmua.a@gmail.com>
2018-12-20 10:58:36 +0700

    add foder

---
+++ TIM/src/bw0-parent/bw0-web/src/test/java/th/co/toyota/bw0/web/customer/TestCustomerList.java
@@ -0,0 +1,14 @@
+package th.co.toyota.bw0.web.customer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCustomerList {
+
+	@Test
+	public void searchCustomerExpireDataShouldBeEmtey() {
+		//searchCustomerExpireData
+	}
+
+}
