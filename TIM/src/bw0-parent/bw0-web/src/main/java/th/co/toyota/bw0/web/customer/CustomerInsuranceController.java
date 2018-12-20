61188005d52bb6f071a386ba6c5d9f9635d2b70c
yong <yongsukanya@gmail.com>
2018-12-20 11:54:42 +0700

    modify customer insurance control

--- TIM/src/bw0-parent/bw0-web/src/main/java/th/co/toyota/bw0/web/customer/CustomerInsuranceController.java
+++ TIM/src/bw0-parent/bw0-web/src/main/java/th/co/toyota/bw0/web/customer/CustomerInsuranceController.java
@@ -1,6 +1,8 @@
 package th.co.toyota.bw0.web.customer;
 
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Validator;
 
@@ -12,21 +14,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.support.RequestContext;
-import org.springframework.web.servlet.support.RequestContextUtils;
 
+import th.co.toyota.application.model.CustomerInsuranceInfo;
+import th.co.toyota.application.model.InsuranceInfo;
 import th.co.toyota.application.model.Payload;
 import th.co.toyota.application.model.ServiceStatus;
 import th.co.toyota.application.model.XmlPayload;
 import th.co.toyota.application.web.BaseController;
 import th.co.toyota.bw0.api.constants.AppConstants;
 import th.co.toyota.bw0.api.exception.common.CommonErrorException;
-import th.co.toyota.bw0.api.repository.common.IBW00000Repository;
 import th.co.toyota.bw0.api.service.common.CBW00000CommonService;
 import th.co.toyota.bw0.api.service.common.UserInfo;
-import th.co.toyota.bw0.web.customer.form.CustomerInsuranceForm;
 import th.co.toyota.bw0.web.customer.service.CustomerInsuranceService;
 import th.co.toyota.bw0.web.master.form.CST33060Form;
 
@@ -43,32 +42,35 @@ public class CustomerInsuranceController extends BaseController {
 	@Autowired
 	protected CBW00000CommonService commonService;
 
-	@Autowired
-	private IBW00000Repository commonRepository;
 	
 	@Autowired
 	private CustomerInsuranceService service;
 
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView initial(HttpServletRequest request, CST33060Form form) throws CommonErrorException {
+	public ModelAndView initial(HttpServletRequest request, CST33060Form form, @RequestParam String customerId
+			, @RequestParam String insuranceType) throws CommonErrorException {
 		logger.info("Initial form is initated.");
 		
 		ModelAndView mv = new ModelAndView(VIEW_NAME);
 		Payload payload = new XmlPayload();
 		ServiceStatus status = ServiceStatus.OK;
-		try {	
-			payload = populatePayloadForDisplay(VIEW_NAME, payload, RequestContextUtils.getLocale(request));
+		try {
 			payload.setStatus(ServiceStatus.OK);
 			UserInfo userInfo = getUserInSession(request);
+			service.searchAllData(customerId, insuranceType, payload);
+			
+			CustomerInsuranceInfo custInfo = (CustomerInsuranceInfo)payload.getObjectInfo();
+			
+			List<InsuranceInfo> insuranceList = payload.getObjectsInfoList();
+
 			
 			mv.addObject(AppConstants.MV_USER, userInfo);
 			mv.addObject(AppConstants.MV_FORM, form);
 			mv.addObject(AppConstants.MV_PAYLOAD, payload);
-			
-			int rowsPerPage = commonRepository.getRowPerPage(VIEW_NAME);
-			form.setRowsPerPage(rowsPerPage);
-			
+			mv.addObject("customerInfo", custInfo);
+			mv.addObject("insuranceList", insuranceList);
+
 		} catch (Exception e) {
 			logger.error(ExceptionUtils.getStackTrace(e));
 			status = ServiceStatus.NG;
@@ -76,29 +78,4 @@ public class CustomerInsuranceController extends BaseController {
 		payload.setStatus(status);
 		return mv;
 	}
-	
-	@RequestMapping(value="/search", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody Object searchData(CustomerInsuranceForm form, HttpServletRequest request, RequestContext context, @RequestParam String customerId) {
-		
-		Payload payload = new XmlPayload();
-		ServiceStatus status = ServiceStatus.OK;
-		try{
-			payload = populatePayloadForDisplay(VIEW_NAME, payload, RequestContextUtils.getLocale(request));
-			form.getCustomerId();
-			boolean isFound = service.searchAllData(form.getCustomerId(), form.getInsuranceType(), payload);
-
-			if (!isFound) {
-				status = ServiceStatus.NG;
-			}
-
-		} catch (Exception e) {
-			logger.error(ExceptionUtils.getStackTrace(e));
-			status = ServiceStatus.NG;
-		}
-		payload.setStatus(status);
-		return payload;
-		
-	}
-	
-	
 }
